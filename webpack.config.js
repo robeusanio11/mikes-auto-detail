93b0361a9323b2a2097ec403f3a7c50e0e5a636b
robeusanio11 <robeusanio11@gmail.com>
2021-04-14 09:28:01 -0700

    front-end setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,23 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
+  mode: "development",
   entry: './client/src/index.js',
   output: {
-    path: path.resolve(__dirname, '/client/public'),
+    path: path.resolve(__dirname, './client/public'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+},
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
   }
 };
 
