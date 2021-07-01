ef92068416c0dcd86380353a19190e0859360cd4
robeusanio11 <robeusanio11@gmail.com>
2021-07-01 15:27:04 -0700

    checked/more-less functionality

--- client/src/components/AppointmentJob.jsx
+++ client/src/components/AppointmentJob.jsx
@@ -1,15 +1,66 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react';
 
-const AppointmentJob = ({ job }) => (
+const AppointmentJob = ({ job }) => {
   const [clicked, setClicked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-//   <div class="job-list-entry-container">
+  useEffect(() => {
+    console.log(clicked, checked);
+  })
+
+
+
+  if (clicked) {
+    return (
+      <div
+        class="job-list-entry-container"
+        onClick={() => { setChecked(!checked); }}
+      >
+        <div class="job-list-entry">
+          <h4>{job.title}</h4>
+          <p class="job-list-entry-description">{job.description}</p>
+          <p class="more-less" onClick={() => setClicked(!clicked)}>See Less</p>
+        </div>
+        <input type="checkbox" value={checked} checked={checked} />
+      </div>
+    );
+  }
+  return (
+    <div
+      class="job-list-entry-container"
+      onClick={() => { setChecked(!checked); }}
+    >
+      <div class="job-list-entry">
+        <h4>{job.title}</h4>
+        <p
+          class="more-less"
+          onClick={() => { setClicked(!clicked); }}
+        >More Info</p>
+      </div>
+      <input type="checkbox" value={checked} checked={checked} />
+    </div>
+  );
+}
+
+
+
+
+// return (
+//   <div
+//     class="job-list-entry-container"
+//     onClick={() => { setChecked(!checked); }}
+//   >
 //     <div class="job-list-entry">
 //       <h4>{job.title}</h4>
-//       <p class="more-less" onClick={() => setClicked(!clicked)}>More Info</p>
+
+//       <p
+//         class="more-less"
+//         onClick={() => { setClicked(!clicked); }}
+//       >More Info</p>
 //     </div>
-//     <span>Book Now</span>
+//     <input type="checkbox" value={checked} checked={checked} />
 //   </div>
 // );
+// };
 
 export default AppointmentJob;
