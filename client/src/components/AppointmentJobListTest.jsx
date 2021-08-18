58be44c334e837885187f65e042a175d2baa6a5f
robeusanio11 <robeusanio11@gmail.com>
2021-08-18 15:41:47 -0700

    modal styling body/header

--- client/src/components/AppointmentJobListTest.jsx
+++ client/src/components/AppointmentJobListTest.jsx
@@ -3,18 +3,30 @@ import AppointmentJob from './AppointmentJobTest.jsx';
 
 const AppointmentJobList = ({ jobs }) => (
   <div id="myModal" class="modal">
-  <div class="modal-content">
-    <p>Some text in the Modal..</p>
+    <div class="modal-content">
+      <header
+        id="appointmentModalHeader"
+        class="modal-header"
+      >
+        <img src='https://d1g145x70srn7h.cloudfront.net/files/bfdb6e9d33911b56e830e95a712b73a0b31ff36f/original.jpeg' id="modal-title-image" />
+        <div id='appointmentModalTitle'>Mike's Auto Detail</div>
+        <div id='appointmentModalSignIn'>Sign In</div>
+      </header>
+
+      <div class="appointment-modal-content">
+      <div class="modal-sidebar">test</div>
+      <ul class="modal-appopintment-list">
+        {
+          jobs.map((job) => (
+            <AppointmentJob job={job} />
+          ))
+        }
+      </ul>
+      </div>
+    </div>
+
   </div>
 
-</div>
-  // <ul>
-  //   {
-  //     jobs.map((job) => (
-  //       <AppointmentJob job={job} />
-  //     ))
-  //   }
-  // </ul>
 );
 
 export default AppointmentJobList;
--- client/src/components/AppointmentJobTest.jsx
+++ client/src/components/AppointmentJobTest.jsx
@@ -8,81 +8,72 @@ const AppointmentJob = ({ job }) => {
     console.log(clicked, checked);
   })
 
-
-  return (
-    <>
-    <p>hello</p>
-    </>
-  )
+    if (clicked) {
+      return (
+        <div
+          class="job-list-entry-container"
+          onClick={(e) => {
+            if (e.target.className !== 'more-less' && e.target.className !== 'job-options-select') {
+              setChecked(!checked);
+            }
+          }}
+        >
+          <div class="job-list-entry">
+            <h4>{job.title}</h4>
+            {!checked &&
+              <span>{job.options.length} Options</span>
+            }
+            <span class="more-less" onClick={() => setClicked(!clicked)}>See Less</span>
+            <p class="job-list-entry-description">{job.description}</p>
+            {checked &&
+              <select class="job-options-select">
+                {job.options.map((option) =>
+                  (<option value={option}>{option}</option>)
+                )}
+              </select>
+            }
+          </div>
+          <input type="checkbox" value={checked} checked={checked} />
+        </div>
+      );
+    }
+    return (
+      <div
+        class="job-list-entry-container"
+        onClick={(e) => {
+          console.log(e.target.className)
+          if (e.target.className !== 'more-less' && e.target.className !== 'job-options-select') {
+            setChecked(!checked);
+          }
+        }}
+      >
+        <div class="job-list-entry">
+          <h4>{job.title}</h4>
+          <div class="job-list-entry-options">
+            {!checked &&
+              <span>{job.options.length} Options</span>
+            }
+            <span
+              class="more-less"
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+            >More Info</span>
+            {checked &&
+              <>
+              <br></br>
+              <select class="job-options-select">
+                {job.options.map((option) =>
+                  (<option value={option}>{option}</option>)
+                )}
+              </select>
+            </>
+            }
+          </div>
+        </div>
+        <input type="checkbox" value={checked} checked={checked} />
+      </div>
+    );
   }
-
-
-  //   if (clicked) {
-  //     return (
-  //       <div
-  //         class="job-list-entry-container"
-  //         onClick={(e) => {
-  //           if (e.target.className !== 'more-less' && e.target.className !== 'job-options-select') {
-  //             setChecked(!checked);
-  //           }
-  //         }}
-  //       >
-  //         <div class="job-list-entry">
-  //           <h4>{job.title}</h4>
-  //           {!checked &&
-  //             <span>{job.options.length} Options</span>
-  //           }
-  //           <span class="more-less" onClick={() => setClicked(!clicked)}>See Less</span>
-  //           <p class="job-list-entry-description">{job.description}</p>
-  //           {checked &&
-  //             <select class="job-options-select">
-  //               {job.options.map((option) =>
-  //                 (<option value={option}>{option}</option>)
-  //               )}
-  //             </select>
-  //           }
-  //         </div>
-  //         <input type="checkbox" value={checked} checked={checked} />
-  //       </div>
-  //     );
-  //   }
-  //   return (
-  //     <div
-  //       class="job-list-entry-container"
-  //       onClick={(e) => {
-  //         console.log(e.target.className)
-  //         if (e.target.className !== 'more-less' && e.target.className !== 'job-options-select') {
-  //           setChecked(!checked);
-  //         }
-  //       }}
-  //     >
-  //       <div class="job-list-entry">
-  //         <h4>{job.title}</h4>
-  //         <div class="job-list-entry-options">
-  //           {!checked &&
-  //             <span>{job.options.length} Options</span>
-  //           }
-  //           <span
-  //             class="more-less"
-  //             onClick={() => {
-  //               setClicked(!clicked);
-  //             }}
-  //           >More Info</span>
-  //           {checked &&
-  //             <>
-  //             <br></br>
-  //             <select class="job-options-select">
-  //               {job.options.map((option) =>
-  //                 (<option value={option}>{option}</option>)
-  //               )}
-  //             </select>
-  //           </>
-  //           }
-  //         </div>
-  //       </div>
-  //       <input type="checkbox" value={checked} checked={checked} />
-  //     </div>
-  //   );
-  // }
 
   export default AppointmentJob;
